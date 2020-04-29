85c5cfcd1a6008e7cc8e3cdca0cfc6661d1c7333
Austin Cory Bart <acbart@vt.edu>
2020-04-29 14:13:04 -0400

    Updates

---
+++ dev/precache-manifest.3e086415e601a1021519c2ff5b812a1e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893ada6623142af3f5bbf20e23d5ebb2",
+    "url": "/educational-scene-changer/devassets/ada.png"
+  },
+  {
+    "revision": "fae3f6531c250f6a1f1e4984a9471860",
+    "url": "/educational-scene-changer/devassets/campus.png"
+  },
+  {
+    "revision": "e7087d5d91321d575b038366ef9f2407",
+    "url": "/educational-scene-changer/devassets/citations.md"
+  },
+  {
+    "revision": "4ec18a8325ffb6d970c2760ba45923e2",
+    "url": "/educational-scene-changer/devassets/east_campus.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-scene-changer/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/educational-scene-changer/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/educational-scene-changer/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/educational-scene-changer/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/educational-scene-changer/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "9859fffc506f3c6e5372f93f9a948885",
+    "url": "/educational-scene-changer/devassets/potato.png"
+  },
+  {
+    "revision": "75dad167a793618cfd4bbf267943d3fb",
+    "url": "/educational-scene-changer/devassets/smith.png"
+  },
+  {
+    "revision": "af2aa279f355dd8f1400672f323058ab",
+    "url": "/educational-scene-changer/devassets/soon.png"
+  },
+  {
+    "revision": "3d7449c591f6334f15148690e1e90623",
+    "url": "/educational-scene-changer/devassets/trabant.png"
+  },
+  {
+    "revision": "34761f7f09b4a64431b03fdab73ca9d7",
+    "url": "/educational-scene-changer/devassets/whiteboard_corgi.png"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/educational-scene-changer/devfavicon.ico"
+  },
+  {
+    "revision": "b70f8e169154bf5b18f3",
+    "url": "/educational-scene-changer/devgen.main.4c377a61c02381236257.bundle.js"
+  },
+  {
+    "revision": "1fd1450cb4cee73f2d88",
+    "url": "/educational-scene-changer/devgen.vendors.bebab0813e6f9e4f971d.bundle.js"
+  },
+  {
+    "revision": "d71cee318ab581f321fd1c82953092de",
+    "url": "/educational-scene-changer/devindex.html"
+  },
+  {
+    "revision": "003187cab0ecf7a20e343e2ba6db4a34",
+    "url": "/educational-scene-changer/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/educational-scene-changer/devsw.js"
+  }
+]);
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-scene-changer/devprecache-manifest.3e086415e601a1021519c2ff5b812a1e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
